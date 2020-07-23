e81c16b2c506e09f86566043b643872bde10a827
M1M2-coder <m1m2.coder@gmail.com>
2020-07-23 03:11:16 +0300

    Successfully Loaded the static resources, and setting the Landing Page

---
+++ onlineshopping/src/main/java/com/m2m/onlineshopping/controller/PageController.java
@@ -0,0 +1,19 @@
+package com.m2m.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value = {"/","/home","/index"})
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title","Home");
+		mv.addObject("greeting","Hi Mr Mahmoud");
+
+		return mv;
+	}
+	
+}
